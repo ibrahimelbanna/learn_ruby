0b0e20118fbfbc3555792e75a8e3efc774642858
ibrahimelbanna <proghima54@gmail.com>
2019-01-12 14:57:55 +0200

    Adding

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,12 @@
 #write your code here
+ Vowels = ['a', 'e', 'u', 'o', 'i']
+def translate string
+changedWords = string.split.map do |word|
+    firstVowelIndex = word.index(/[aeiou]/)
+    if word[firstVowelIndex-1, 2] == "qu"
+      firstVowelIndex += 1
+    end
+    word.chars.rotate(firstVowelIndex).join + "ay"
+  end
+  changedWords.join(" ")
+end
