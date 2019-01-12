a781d863aa34386ea876b737d2ddc97abea5db1f
ibrahimelbanna <proghima54@gmail.com>
2019-01-12 11:36:15 +0200

    Adding calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,30 @@
 #write your code here
+def add(a,b)
+    a+b
+end
+
+def subtract(a,b)
+    a-b
+end
+
+def sum(array)
+    array.inject(0){|sum,x| sum+=x}
+end
+
+def multiply(a)
+    product = 1
+    a.each {|x| product*= x}
+    product
+end
+
+def power(a,b)
+    a**b
+end
+
+def factorial(n)
+     if n  == 0 
+        1
+     else
+        n*factorial(n-1)
+     end
+end
