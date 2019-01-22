44cc885a340bfb4ceb3c54c90306b7c7c4588bb8
ibrahimelbanna <proghima54@gmail.com>
2019-01-22 10:09:43 +0200

    solving the book

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,17 @@
 class Book
 # write your code here
+ attr_accessor :title
+
+def title=(t)
+     arr = [] 
+     expation_words = %w(and the over in of a an)
+     t.split(' ').each_with_index do |word,i|
+        if expation_words.include? word and i !=0
+           arr << word
+       else 
+        arr <<  word.capitalize
+       end
+     end
+    @title = arr.join(' ')
+  end
 end
