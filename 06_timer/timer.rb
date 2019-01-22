211ff3871e4dfc163eeda70007b94f7cfaf0c70f
ibrahimelbanna <proghima54@gmail.com>
2019-01-22 10:27:50 +0200

    Afding the timeer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,19 @@
 class Timer
   #write your code here
+  attr_accessor :seconds, :minutes, :hours
+  def initialize
+    @seconds = 0
+    @minutes = 0
+    @hours = 0 
+
+  end
+  def time_string
+    @hours = @seconds/3600
+    @minutes = @seconds%3600/60
+    @seconds =@seconds%60
+
+    sprintf("%02d:%02d:%02d", @hours, @minutes, @seconds)
+  end
+  
+  
 end
